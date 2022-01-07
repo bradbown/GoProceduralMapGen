276e52457a6d121df05122f7a94c5cfd1e670747
Bradley Bown <bradbown@googlemail.com>
2022-01-07 19:00:57 +0000

    Setting up new fresh api using fiber

---
+++ procmap-api/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/api", welcome)
+
+	log.Fatal(app.Listen(":3000"))
+}
+
+func welcome(c *fiber.Ctx) error {
+	return c.SendString("Welcome to my awesome API")
+}
