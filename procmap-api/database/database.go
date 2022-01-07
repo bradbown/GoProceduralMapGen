0fe5e58bb42787dada3236a83be1c91003ee9d3c
Bradley Bown <bradbown@googlemail.com>
2022-01-07 20:06:12 +0000

    Setup the db

---
+++ procmap-api/database/database.go
@@ -0,0 +1,34 @@
+package database
+
+import (
+	"log"
+	"os"
+
+	"github.com/bradbown/GoProceduralMapGen/models"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type DbInstance struct {
+	Db *gorm.DB
+}
+
+var Database DbInstance
+
+func ConnectDb() {
+	db, err := gorm.Open(sqlite.Open("procmapgen.db"), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Failed to connect to the database! \n", err.Error())
+		os.Exit(2)
+	}
+
+	log.Println("Connected to database successfully")
+	db.Logger = logger.Default.LogMode(logger.Info)
+	log.Println("Running Migrations")
+
+	db.AutoMigrate(&models.User{}, &models.Product{})
+
+	Database = DbInstance{Db: db}
+}
--- procmap-api/main.go
+++ procmap-api/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"log"
 
+	"github.com/bradbown/GoProceduralMapGen/database"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	database.ConnectDb()
 	app := fiber.New()
 
 	app.Get("/api", welcome)
