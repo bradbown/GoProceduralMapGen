0e164d3c971f0e364bde595b4f3968eb5f60ae35
Bradley Bown <bradbown@googlemail.com>
2022-01-09 21:53:18 +0000

    Created database entries for noisemap

--- map.go
+++ map.go
@@ -19,26 +19,21 @@
 // 	scale         float32
 // }
 
-// func GenerateNoiseMap() {
-
-// 	mapGen := Map{
-// 		width:  10,
-// 		height: 10,
-// 		scale:  2,
-// 	}
-
-// 	//var noiseMap [10][10]float64
-
-// 	for x := 0; x < mapGen.width; x++ {
-// 		for y := 0; y < mapGen.height; y++ {
-
-// 			perlinValue := perlin.Noise2D(float64(x), float64(y), int64(seed), alpha, beta, octaves)
-// 			perlinValue = (perlinValue * .5) + .5
-// 			perlinValue = math.Max(0.0, perlinValue)
-// 			perlinValue = math.Min(1.0, perlinValue)
-
-// 			//noiseMap[x][y] = perlinValue
-// 			fmt.Println(perlinValue)
-// 		}
-// 	}
-// }
+func GenerateNoiseMap() {
+	mapGen := Map{
+		width:  10,
+		height: 10,
+		scale:  2,
+	}
+	//var noiseMap [10][10]float64
+	for x := 0; x < mapGen.width; x++ {
+		for y := 0; y < mapGen.height; y++ {
+			perlinValue := perlin.Noise2D(float64(x), float64(y), int64(seed), alpha, beta, octaves)
+			perlinValue = (perlinValue * .5) + .5
+			perlinValue = math.Max(0.0, perlinValue)
+			perlinValue = math.Min(1.0, perlinValue)
+			//noiseMap[x][y] = perlinValue
+			fmt.Println(perlinValue)
+		}
+	}
+}
