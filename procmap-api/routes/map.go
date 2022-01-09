24bb7a2d1b72361717e7e22c9d274f8607eaf6c1
Bradley Bown <bradbown@googlemail.com>
2022-01-09 00:02:05 +0000

    Created response map

--- procmap-api/routes/map.go
+++ procmap-api/routes/map.go
@@ -1,8 +1,14 @@
 package routes
 
+import "github.com/bradbown/GoProceduralMapGen/models"
+
 type Map struct {
 	ID      uint    `json:"id"`
 	Seed    uint    `json:"seed"`
 	Alpha   float32 `json:"alpha"`
 	Octaves uint    `json:"octaves"`
+}
+
+func CreateResponseMap(mapM models.Map) Map {
+	return Map{ID: mapM.ID, Seed: mapM.Seed, Alpha: mapM.Alpha, Octaves: mapM.Octaves}
 }
